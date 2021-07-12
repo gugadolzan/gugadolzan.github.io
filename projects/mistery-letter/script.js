44a7ac17b013ae11f2dda5892f7f36260275e19e
Gustavo Dolzan <gudolzan@gmail.com>
2021-07-11 21:56:14 -0300

    Project Mistery Letter

---
+++ projects/mistery-letter/script.js
@@ -0,0 +1,44 @@
+const input = document.getElementById('carta-texto');
+const inputButton = document.getElementById('criar-carta');
+const outputParagraph = document.getElementById('carta-gerada');
+const wordCounter = document.getElementById('carta-contador');
+const classes = [ // An empty string for each class for none class
+  ['newspaper', 'magazine1', 'magazine2'],
+  ['medium', 'big', 'reallybig'],
+  ['rotateleft', 'rotateright'],
+  ['skewleft', 'skewright'],
+];
+
+function randomClasses(letter) {
+  for (let index = 0; index < classes.length; index += 1) {
+    if (Math.random() < 0.5) { // 50/50 condition
+      letter.classList.add(classes[index][Math.floor(Math.random() * (classes[index].length))]); // Adds a random class from classes array if previous condition is true
+    }
+  }
+}
+
+function printsLetter(outputArray) {
+  for (let index = 0; index < outputArray.length; index += 1) {
+    const outputLetter = document.createElement('span');
+    outputLetter.innerText = outputArray[index];
+    while (outputLetter.classList.length < 2) { // Condition for at least two classes
+      randomClasses(outputLetter);
+    }
+    outputParagraph.appendChild(outputLetter);
+  }
+}
+
+function generateLetter() {
+  inputButton.addEventListener('click', () => {
+    if (!input.value.trim()) { // .trim() removes beginning and endind whitespaces
+      outputParagraph.innerText = 'Por favor, digite o conteúdo da carta.';
+      wordCounter.innerText = 0;
+    } else {
+      outputParagraph.innerHTML = ''; // Clears past outputs
+      const outputArray = input.value.trim().split(' '); // Turns input into array
+      wordCounter.innerText = outputArray.length;
+      printsLetter(outputArray);
+    }
+  });
+}
+generateLetter();
